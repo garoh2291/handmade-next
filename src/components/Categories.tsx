84e1932228840f907bfc26744a5feca06017a729
Hayk Tsaturyan <haykcaturyan@gmail.com>
2025-01-31 21:10:39 +0400

    Add Categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,6 +1,35 @@
 "use client";
+import { categories } from "@/constants/categories";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Categories() {
-  return <div>Categories</div>;
+  return (
+    <div className="w-full h-36 flex flex-row justify-between items-center px-5">
+      {categories.map(
+        (value: { url: string; img: string; name: string }, index: number) => {
+          return (
+            <div key={index}>
+              <Link href={value.url}>
+                <div className="flex flex-row items-center gap-2 hover:text-hm-yellow">
+                  <div className="relative w-24 h-24">
+                    <Image
+                      src={value.img}
+                      alt={value.name}
+                      fill
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div>
+                    <span>{value.name}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
---
+++ src/constants/categories.ts
@@ -0,0 +1,27 @@
+export const categories = [
+  {
+    url: "/category1",
+    img: "/assets/categories/1.png",
+    name: "Category 1",
+  },
+  {
+    url: "/category2",
+    img: "/assets/categories/2.png",
+    name: "Category 2",
+  },
+  {
+    url: "/category3",
+    img: "/assets/categories/3.png",
+    name: "Category 3",
+  },
+  {
+    url: "/category4",
+    img: "/assets/categories/4.png",
+    name: "Category 4",
+  },
+  {
+    url: "/category5",
+    img: "/assets/categories/5.png",
+    name: "Category 5",
+  },
+];
